7c4caed3be922b9e892ea2f2846a34661fce3358
unknown <leonardo.arce@ucb.edu.bo>
2026-03-18 20:18:40 -0400

    Fix: Non-nullable property warnings in DTOs and added sonar workflow

--- Models/DTOS/CreateRecursoDto.cs
+++ Models/DTOS/CreateRecursoDto.cs
@@ -9,11 +9,5 @@ namespace ImpulseClub.Models.DTOS
 
         [Required]
         public required string Tipo { get; init; }
-
-        [Range(1, 10000)]
-        public int CantidadTotal { get; init; }
-
-        [Required]
-        public Guid ClubId { get; init; }
     }
 }
